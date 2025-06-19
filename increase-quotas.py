a917ed8bd002e64c8b226bba53caa53446edabb1
ARYAN <aryan.kulshrestha@cloudkeeper.com>
2025-06-19 11:39:32 +0530

    Changed the Names of file according to ReadMe

--- increase-quotas.py
+++ increase-quotas.py
@@ -6,7 +6,5 @@ response = client.list_service_quotas(
     ServiceCode='organizations',
     QuotaCode='L-E619E033'
 )
-# data1 = json.loads(response['Quotas']['Value'])
 data =response["Quotas"][0]
 print(data["Value"])
-# print(response['Quotas']['Value'])
